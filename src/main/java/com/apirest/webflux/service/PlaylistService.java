b9e021e47493179f6fc28873741c1ed9e0ca4219
robsonjsdiniz <robsonjsdiniz@gmail.com>
2025-06-17 17:50:42 -0300

    Add files via upload

---
+++ src/main/java/com/apirest/webflux/service/PlaylistService.java
@@ -0,0 +1,15 @@
+package com.apirest.webflux.service;
+
+import com.apirest.webflux.document.Playlist;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface PlaylistService {
+
+	Flux<Playlist> findAll();
+	Mono<Playlist> findById(String id);
+	Mono<Playlist> save(Playlist playlist);
+	Mono<Void> deleteById(String id);
+	
+}
